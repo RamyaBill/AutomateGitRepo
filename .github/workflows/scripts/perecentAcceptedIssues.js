a865a61b4fb1858ef1c294d7e171060ac7d4edba
RamyaBill <95648778+RamyaBill@users.noreply.github.com>
2024-05-01 11:43:14 +0100

    Update and rename filterAcceptedIssues.js to perecentAcceptedIssues.js

---
+++ .github/workflows/scripts/perecentAcceptedIssues.js
@@ -0,0 +1,31 @@
+module.exports = async ({github, context, core}) => {
+    const { owner, repo } = context.repo;
+    const acceptLabel = "Status: Accepted";
+
+    // Get dates for last month
+    const first_day = new Date();
+    first_day.setMonth(first_day.getMonth() - 1);
+    first_day.setDate(1);
+
+      // Query all issues created in the last month
+     const lastMonthIssuesResponse = await github.rest.issues.listForRepo({
+        owner,
+        repo,
+        since: first_day.toISOString().slice(0, 10),
+        state: 'all'
+     });
+
+    const lastMonthIssues = lastMonthIssuesResponse.data;
+
+    // Filter the last month issues to find accepted issues
+    const acceptedIssues = lastMonthIssues.filter(issue => {
+        return issue.labels.some(label => label.name === acceptLabel);
+    });
+
+    const totalIssues = lastMonthIssues.length;
+  
+    // Calculate the percentage of accepted issues
+    const percentage = ((acceptedIssues.length / totalIssues) * 100).toFixed(2);
+
+    console.log(`Percentage of accepted issues in the last month: ${percentage}%`);
+};
